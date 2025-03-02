f327deeb0e68293cd4122a130ded67be234c10d1
Trung2204 <minhtrung9a8lhpqn@gmail.com>
2025-03-02 23:01:11 +0700

    feature: navigation bar, footer

---
+++ src/app/components/MenuOverlay.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import NavLink from "./NavLink";
+import { LinkType } from "../types";
+
+interface MenuOverlayProps {
+  links: LinkType[];
+}
+
+const MenuOverlay: React.FC<MenuOverlayProps> = ({ links }) => {
+  return (
+    <ul className="flex flex-col py-4 items-center">
+      {links.map((link, index) => (
+        <li key={index}>
+          <NavLink href={link.href} title={link.title} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MenuOverlay;
